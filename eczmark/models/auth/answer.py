c7ea9ec34e3e093ac49faf6b514c87a52332a956
chriford <siamechrif@gmail.com>
2023-07-31 23:35:24 +0200

    implement the answer model

---
+++ eczmark/models/auth/answer.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+from eczmark.models.abstract import Timestamp
+from django.core.validators import MaxLengthValidator, MinLengthValidator
+
+
+class Answer(Timestamp):
+    user = models.ForeignKey(
+        to="eczmark.User",
+        verbose_name="Marker",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    attachments = models.ManyToManyField(
+        to="eczmark.Attachment",
+        verbose_name="Supporting images",
+        null=True,
+        blank=True,
+    )
+    links = models.ManyToManyField(
+        to="eczmark.Link",
+        verbose_name="Supporting link",
+        null=True,
+        blank=True,
+    )
+    body = models.TextField(
+        null=True,
+        blank=False,
+        validators=[
+            MinLengthValidator(50),
+            MaxLengthValidator(1200),
+        ],
+    )
+    
+    def __str__(self):
+        if self.user:
+            full_name = f"{self.user.first_name} {self.user.last_name}"
+            return full_name
+        else:
+            return self.user.username
