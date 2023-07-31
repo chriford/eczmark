a9e39f36cabb082a9bb31b676648a509b1b26fcd
chriford <siamechrif@gmail.com>
2023-07-31 23:42:50 +0200

    implement the report model

---
+++ eczmark/models/auth/report.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+from eczmark.models.abstract import Timestamp
+from django.core.validators import MaxLengthValidator, MinLengthValidator
+
+class Report(Timestamp):
+    user = models.ForeignKey(
+        to="eczmark.User",
+        verbose_name="Marker",
+        on_delete=models.SET_NULL,
+        default=True,
+        null=False,
+        blank=True,
+    )
+    issue = models.ForeignKey(
+        to="eczmark.Issue",
+        verbose_name="Title",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+    )
+    message = models.TextField(
+        max_length=350,
+        verbose_name="Message body",
+        help_text="Report an issue to the support team",
+        null=True,
+        blank=False,
+    )
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.issue.name
