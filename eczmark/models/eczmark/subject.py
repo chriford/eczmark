a6f577e913ebcd7c1c9026854f3ca49d8b0daef9
chriford <siamechrif@gmail.com>
2023-08-01 00:20:45 +0200

    update a subject model attribute

--- eczmark/models/eczmark/subject.py
+++ eczmark/models/eczmark/subject.py
@@ -6,6 +6,7 @@ class Subject(Timestamp):
     name = models.CharField(
         verbose_name="Subject",
         help_text="The subject of the question paper e.g English, Science, ...",
+        max_length=100,
         null=True,
         blank=False,
     )
