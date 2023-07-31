ab544decf53ecdfde5e3fdb768b59eb30886a399
chriford <siamechrif@gmail.com>
2023-07-31 23:40:51 +0200

    implement the grade model

---
+++ eczmark/models/auth/grade.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from eczmark.models.abstract import Timestamp
+from django.core.validators import MaxLengthValidator, MinLengthValidator
+
+class Grade(Timestamp):
+    grade = models.SmallIntegerField(
+        verbose_name="Grade",
+        null=True,
+        blank=False,
+    )
+
+    def __str__(self):
+        return self.grade
