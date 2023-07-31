e89f6d9d24a2c8150afb52c914cc5d3e7eeeab04
chriford <siamechrif@gmail.com>
2023-07-31 09:36:55 +0200

    register the main app

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'eczmark',
+    'eczmark.apps.EczmarkConfig',
 ]
 
 MIDDLEWARE = [
