935c0c9ca5d723d749f91ad3a90e756085fc341e
chriford <siamechrif@gmail.com>
2023-08-06 15:52:42 +0200

    cleanup the serializer class

--- eczmark/restful_api/serializers/question.py
+++ eczmark/restful_api/serializers/question.py
@@ -1,9 +1,31 @@
 
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
-from eczmark.models import Question
+from eczmark.models import Question, Grade
+from eczmark.restful_api.serializers import (
+    SubjectSerializer,
+    GradeSerializer,
+)
 
-class QuestionSerializer(ModelSerializer):
+class QuestionSerializer(serializers.ModelSerializer):
+    grade = serializers.SerializerMethodField()
+    subject = serializers.SerializerMethodField()
+
     class Meta:
         model = Question
-        fields = '__all__'
+        fields = [
+            'user',
+            'question_paper',
+            'grade',
+            'year_uncleaned',
+            'cleaned_year',
+            'subject',
+        ]
+    
+    def get_subject(self, instance):
+        serializer = SubjectSerializer(instance.subject, many=False)
+        return serializer.data
+    
+    def get_grade(self, instance):
+        serializer = GradeSerializer(instance.grade, many=False)
+        return serializer.data
