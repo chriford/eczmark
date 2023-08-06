b02c38c7b4cbb1341424932ffe05a7f2c3766395
chriford <siamechrif@gmail.com>
2023-08-06 08:59:53 +0200

    register other model viewsets

--- eczmark/routers.py
+++ eczmark/routers.py
@@ -2,9 +2,13 @@ from rest_framework.routers import DefaultRouter
 
 from eczmark.restful_api.viewsets  import (
     AnswerViewSet,
+    QuestionViewSet,
+    ReportViewSet,
 )
 
 router = DefaultRouter()
+router.register(r'questions', AnswerViewSet, basename='question')
 router.register(r'answers', AnswerViewSet, basename='answer')
+router.register(r'reports', AnswerViewSet, basename='report')
 
 urlpatterns = router.urls
