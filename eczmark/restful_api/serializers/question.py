0e7e8c4e8c37b04ee3ee9ac32765ee9cff780344
chriford <siamechrif@gmail.com>
2023-08-07 09:59:20 +0200

    refactor a class import

--- eczmark/restful_api/serializers/question.py
+++ eczmark/restful_api/serializers/question.py
@@ -2,17 +2,16 @@
 from rest_framework.serializers import ModelSerializer
 
 from eczmark.models import Question
-from eczmark.restful_api.serializers import (
-    UserSerializer,
-    SubjectSerializer,
-    GradeSerializer,
-)
+
+from .user import UserSerializer
+from .subject import SubjectSerializer
+from .grade import GradeSerializer
 
 class QuestionSerializer(ModelSerializer):
     user = UserSerializer()
     subject = SubjectSerializer()
     grade = GradeSerializer()
-    
+
     class Meta:
         model = Question
         fields = [
--- eczmark/routers.py
+++ eczmark/routers.py
@@ -4,10 +4,11 @@ from eczmark.restful_api.viewsets  import (
     AnswerViewSet,
     QuestionViewSet,
     ReportViewSet,
+    UserProfileViewSet,
 )
 
 router = DefaultRouter()
-router.register(r'users', ReportViewSet, basename='user')
+router.register(r'users', UserProfileViewSet, basename='user')
 router.register(r'questions', QuestionViewSet, basename='question')
 router.register(r'answers', AnswerViewSet, basename='answer')
 router.register(r'reports', ReportViewSet, basename='report')
