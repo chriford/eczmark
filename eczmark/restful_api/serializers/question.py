a3b83092b11849e1ddc0255c54aeff7ccc68bc21
chriford <siamechrif@gmail.com>
2023-08-07 19:09:04 +0200

    override question serializer create method field

--- eczmark/restful_api/serializers/question.py
+++ eczmark/restful_api/serializers/question.py
@@ -1,7 +1,7 @@
 
 from rest_framework.serializers import ModelSerializer
 
-from eczmark.models import Question
+from eczmark.models import Question, Grade, User, Subject
 
 from .user import UserSerializer
 from .subject import SubjectSerializer
@@ -11,7 +11,7 @@ class QuestionSerializer(ModelSerializer):
     user = UserSerializer()
     subject = SubjectSerializer()
     grade = GradeSerializer()
-
+    
     class Meta:
         model = Question
         fields = [
@@ -22,3 +22,26 @@ class QuestionSerializer(ModelSerializer):
             'cleaned_year',
             'subject',
         ]
+    
+    def create(self, validated_data):
+        user_data = validated_data.pop('user', None)
+        grade_data = validated_data.pop('grade', None)
+        subject_data = validated_data.pop('subject', None)
+
+        if not user_data:
+            raise ValueError("Expected data for unnullable fields")
+        user = User.objects.create(**user_data)
+        
+        if not subject_data:
+            raise ValueError("Expected data for unnullable fields")
+        subject = Subject.objects.create(**subject_data)
+        
+        if not grade_data:
+            raise ValueError("Expected data for unnullable fields")
+        grade = Grade.objects.create(**grade_data)
+        
+        if not validated_data:
+            raise ValueError("Expected data for unnullable fields")
+        question = Question.objects.create(user=user, subject=subject, grade=grade, **validated_data)
+        return question
+        
