8dd40217cbe505fe9e66a134b601fd676841ab1a
Chriford Siame <66867121+chriford@users.noreply.github.com>
2023-08-08 00:48:49 +0200

    do configurations for the email sending feature (#31)

--- core/settings.py
+++ core/settings.py
@@ -157,3 +157,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'eczmark.User'
 ROLEPERMISSIONS_MODULE = "eczmark.roles"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+SERVER_EMAIL = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
