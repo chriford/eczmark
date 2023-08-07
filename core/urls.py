34386d63d5511aa478518b12bc7711acb82a5826
chriford <siamechrif@gmail.com>
2023-08-07 23:56:09 +0200

    enable token authentication

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,11 @@ from django.conf.urls.static import static
 from drf_yasg2 import openapi
 from rest_framework import permissions
 from drf_yasg2.views import get_schema_view
-
+from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 schema_view = get_schema_view(
     openapi.Info(
         title="ECZ Marked API",
@@ -24,7 +28,10 @@ urlpatterns = [
     re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
     path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-    path('', include('eczmark.routers'))
+    path('', include('eczmark.routers')),
+    path('token/get/', views.obtain_auth_token),
+    path('token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
 
 urlpatterns = [
