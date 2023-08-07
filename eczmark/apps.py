542bbb59040f46e62eea8b8a4a03710c1cbd4880
chriford <siamechrif@gmail.com>
2023-08-08 00:12:26 +0200

    implement user signals

--- eczmark/apps.py
+++ eczmark/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class EczmarkConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'eczmark'
+
+    def ready(self):
+        import eczmark.signals
