2270fb41260336d1646774057bd74212500af9ed
chriford <siamechrif@gmail.com>
2023-08-07 09:17:24 +0200

    enable nested data display

--- eczmark/restful_api/serializers/question.py
+++ eczmark/restful_api/serializers/question.py
@@ -2,8 +2,24 @@
 from rest_framework.serializers import ModelSerializer
 
 from eczmark.models import Question
+from eczmark.restful_api.serializers import (
+    UserSerializer,
+    SubjectSerializer,
+    GradeSerializer,
+)
 
 class QuestionSerializer(ModelSerializer):
+    user = UserSerializer()
+    subject = SubjectSerializer()
+    grade = GradeSerializer()
+    
     class Meta:
         model = Question
-        fields = '__all__'
+        fields = [
+            'user',
+            'question_paper',
+            'grade',
+            'year_uncleaned',
+            'cleaned_year',
+            'subject',
+        ]
