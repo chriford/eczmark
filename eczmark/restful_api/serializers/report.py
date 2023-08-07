7ce17fe8c617138ccddc65f197aced631221f25d
chriford <siamechrif@gmail.com>
2023-08-07 19:23:07 +0200

    override report serializer create method field

--- eczmark/restful_api/serializers/report.py
+++ eczmark/restful_api/serializers/report.py
@@ -1,7 +1,7 @@
 
 from rest_framework import serializers
 
-from eczmark.models import Report
+from eczmark.models import Report, User, Issue
 from .user import UserSerializer
 from .issue import IssueSerializer
 
@@ -16,3 +16,23 @@ class ReportSerializer(serializers.ModelSerializer):
             'message',
             'active',
         ]
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user', None)
+        issue_data = validated_data.pop('issue', None)
+
+        if not user_data:
+            raise ValueError("Expected data for unnullable fields")
+        user = User.objects.create(**user_data)
+        
+        try:
+            issue = Issue.objects.get(name=issue_data.name)
+        except Issue.DoesNotExist:
+            issue = Issue.objects.create(**issue_data)
+        
+        if not issue_data:
+            raise ValueError("Expected data for unnullable fields")
+        
+        report = Report.objects.create(user=user, issue=issue, **validated_data)
+        return report
+    
